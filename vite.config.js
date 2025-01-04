50e1e0329bbef2870235efcf506e7f29cf8f493c
Francisco Lopez <lopezf28@gmail.com>
2025-01-04 00:44:52 -0300

    Preparing to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/cyber-portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
